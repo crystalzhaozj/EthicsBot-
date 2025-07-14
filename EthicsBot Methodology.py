41306cd859f00cf391427c31348cb2732135a0f3
crystalzhaozj <zz192@wellesley.edu>
2025-07-14 14:53:20 -0400

    updated setup

--- EthicsBot Methodology.py
+++ EthicsBot Methodology.py
@@ -1,13 +1,22 @@
 
+import plotly as pl
+
 """
-SIMILARITY measures
+JACCARD SIMILARITY: 4 reports, 4 papers 
 """
-# Cosine similarity with TF-IDF
+
+# 1. Read in reports
+
+# 2. For each pair, calculat the Jaccard similarity
+
+# 3. Produce the plot
+
+
+"""
+COSINE SIMILARITY 
+"""
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-# Converting documents into texts (big for loop)
-
 
 # Calculating similarity
 texts = ["Your first text here", "Your firstsecond text here"]
@@ -16,8 +25,26 @@ tfidf_matrix = vectorizer.fit_transform(texts)
 similarity = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])[0][0]
 print(similarity)
 
+### RQ VS INTRO ###
+
+# 1. Read in RQ reports
+
+# 2. Read in Intro reports
+
+# 3. For each paper: for each pair of RQ vs. Intro (RQ minimal vs. Intro minimal, RQ extended vs. 
+# Intro extended, RQ minimal vs. Intro extended, RQ extended vs. Intro minimal), calculate the cosine similarity
+
+### MINIMAL VS EXTENDED ###
+
+# 1. Read in Minimal reports
+
+# 2. Report Extended reports
+
+# 3. For each paper: for each pair of Minimal vs. Extended (RQ minimal vs. RQ extended, Intro minimal vs. 
+# Intro extended), calculate the cosine similarity
+
 """
-Specificity measures
+RARE WORD DENSITY
 """
 # Word frequency
 from wordfreq import zipf_frequency  # Lower zipf = rarer word
